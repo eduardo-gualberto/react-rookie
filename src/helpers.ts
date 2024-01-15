ee5282429c82962d404a939691a9843b42b95f61
Eduardo Gualberto <58743989+eduardo-gualberto@users.noreply.github.com>
2024-01-15 17:24:34 -0300

    refactor: creates helper function to make extracting sttes, effects from source code easier for tests

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,4 +1,5 @@
 import fs from 'fs'
+import { identifyUseEffects, identifyUseStates, processEffectsIntoNodes } from './index'
 
 export const readExampleSourceCode = (withCycle: boolean, exampleNumber: number): string => {
     const folder = withCycle ? 'withCycles' : 'withoutCycles'
@@ -9,3 +10,18 @@ export const readExampleSourceCode = (withCycle: boolean, exampleNumber: number)
         throw new Error(`Error reading source code example: no example${exampleNumber}.txt in folder ${folder}.\n Is it running in the prject's root?\n\n${error}`);
     }
 }
+
+export const examplesCodeBaseResources = (exampleNum: number) => ({
+    withCycle: {
+        sourceCode: readExampleSourceCode(true, exampleNum),
+        states: identifyUseStates(readExampleSourceCode(true, exampleNum)),
+        effects: identifyUseEffects(readExampleSourceCode(true, exampleNum)),
+        nodes: processEffectsIntoNodes(identifyUseEffects(readExampleSourceCode(true, exampleNum)), identifyUseStates(readExampleSourceCode(true, exampleNum)))
+    },
+    withoutCycle: {
+        sourceCode: readExampleSourceCode(false, exampleNum),
+        states: identifyUseStates(readExampleSourceCode(false, exampleNum)),
+        effects: identifyUseEffects(readExampleSourceCode(false, exampleNum)),
+        nodes: processEffectsIntoNodes(identifyUseEffects(readExampleSourceCode(false, exampleNum)), identifyUseStates(readExampleSourceCode(false, exampleNum)))
+    }
+})
--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,5 @@
 import { identifyUseEffects, identifyUseStates, processEffectsIntoNodes, detectCycle } from '@/index'
-import { readExampleSourceCode } from './helpers'
+import { examplesCodeBaseResources, readExampleSourceCode } from './helpers'
 
 test('identifyUseStates: returns empty list when no useStates', () => {
     expect(identifyUseStates("")).toStrictEqual([])
@@ -104,110 +104,53 @@ test('identifyUseEffects: returns every valid useEffect in the source code', () 
   ])
 })
 
-const example1States = [
-  {
-    sourceCodeFullLine: "const [firstName, setFirstName] = useState('Taylor');",
-    stateName: 'firstName',
-    setStateName: 'setFirstName'
-  },
-  {
-    sourceCodeFullLine: "const [lastName, setLastName] = useState('Swift');",
-    stateName: 'lastName',
-    setStateName: 'setLastName'
-  },
-  {
-    sourceCodeFullLine: "const [fullName,setFullName] = useState('')",
-    stateName: 'fullName',
-    setStateName: 'setFullName'
-  }
-]
-const example1Effects = [
-  {
-    sourceCodeFullLine: 'useEffect(() => {\r\n' +
-      "        setFullName(firstName + ' ' + lastName);\r\n" +     
-      '    }, [firstName, lastName]);',
-    effectBody: "setFullName(firstName + ' ' + lastName);\r\n    ",
-    effectDeps: 'firstName, lastName'
-  },
-  {
-    sourceCodeFullLine: 'useEffect(() => {\r\n' +
-      "        setFirstName(fullName.split(' ')[0])\r\n" +
-      '    }, [fullName])\r\n',
-    effectBody: "setFirstName(fullName.split(' ')[0])\r\n    ",    
-    effectDeps: 'fullName'
-  }
-]
-const example1Nodes = processEffectsIntoNodes(example1Effects, example1States)
+const { nodes: nodesExample1 } = examplesCodeBaseResources(1).withCycle
 
 test('processEffectsIntoNodes: check if it returned the right amount of nodes', () => {
-  expect(example1Nodes.length).toBe(2)
+  expect(nodesExample1.length).toBe(2)
 })
 
 test('processEffectsIntoNodes: check if the returned nodes are correctly extracted (effectDeps)', () => {
-  expect(example1Nodes[0].effectDeps).toBe("firstName, lastName")
-  expect(example1Nodes[1].effectDeps).toBe("fullName")
+  expect(nodesExample1[0].effectDeps).toBe("firstName, lastName")
+  expect(nodesExample1[1].effectDeps).toBe("fullName")
 })
 
 test('processEffectsIntoNodes: check if the nodes have the correct amount of edges', () => {
-  expect(example1Nodes[0].edges.length).toBe(1)
-  expect(example1Nodes[1].edges.length).toBe(1)
+  expect(nodesExample1[0].edges.length).toBe(1)
+  expect(nodesExample1[1].edges.length).toBe(1)
 })
 
 test('processEffectsIntoNodes: check if the nodes edges are correctly connected', () => {
-  expect(example1Nodes[0].edges[0].effectDeps).toBe('fullName')
-  expect(example1Nodes[1].edges[0].effectDeps).toBe('firstName, lastName')
+  expect(nodesExample1[0].edges[0].effectDeps).toBe('fullName')
+  expect(nodesExample1[1].edges[0].effectDeps).toBe('firstName, lastName')
 })
 
 test('detectCycle: example1 test if it returns true', () => {
-  expect(detectCycle(example1Nodes)).toBe(true)
+  const { nodes } = examplesCodeBaseResources(1).withCycle
+  expect(detectCycle(nodes)).toBe(true)
 })
-
-const example2SourceCode = readExampleSourceCode(true, 2)
-const example2States = identifyUseStates(example2SourceCode)
-const example2Effects = identifyUseEffects(example2SourceCode)
-
-const example2Nodes = processEffectsIntoNodes(example2Effects, example2States)
 
 test('detectCycle: example2 test if it returns true', () => {
-  expect(detectCycle(example2Nodes)).toBe(true)
+  const { nodes } = examplesCodeBaseResources(2).withCycle
+  expect(detectCycle(nodes)).toBe(true)
 })
-
-const example3SourceCode = readExampleSourceCode(true, 3)
-const example3States = identifyUseStates(example3SourceCode)
-const example3Effects = identifyUseEffects(example3SourceCode)
-
-const example3Nodes = processEffectsIntoNodes(example3Effects, example3States)
 
 test('detectCycle: example3 test if it returns true', () => {
-  expect(detectCycle(example3Nodes)).toBe(true)
+  const { nodes } = examplesCodeBaseResources(3).withCycle
+  expect(detectCycle(nodes)).toBe(true)
 })
-
-const example4SourceCode = readExampleSourceCode(true, 4)
-const example4States = identifyUseStates(example4SourceCode)
-const example4Effects = identifyUseEffects(example4SourceCode)
-
-const example4Nodes = processEffectsIntoNodes(example4Effects, example4States)
 
 test('detectCycle: example4 test if it returns true', () => {
-  expect(detectCycle(example4Nodes)).toBe(true)
+  const { nodes } = examplesCodeBaseResources(4).withCycle
+  expect(detectCycle(nodes)).toBe(true)
 })
-
-const example1FSourceCode = readExampleSourceCode(false, 1)
-const example1FStates = identifyUseStates(example1FSourceCode)
-const example1FEffects = identifyUseEffects(example1FSourceCode)
-
-const example1FNodes = processEffectsIntoNodes(example1FEffects, example1FStates)
 
 test('detectCycle: example1 test if it returns false', () => {
-  expect(detectCycle(example1FNodes)).toBe(false)
+  const { nodes } = examplesCodeBaseResources(1).withoutCycle
+  expect(detectCycle(nodes)).toBe(false)
 })
 
-const example2FSourceCode = readExampleSourceCode(false, 2)
-const example2FStates = identifyUseStates(example2FSourceCode)
-const example2FEffects = identifyUseEffects(example2FSourceCode)
-
-const example2FNodes = processEffectsIntoNodes(example2FEffects, example2FStates)
-
 test('detectCycle: example2 test if it returns false', () => {
-  expect(detectCycle(example2FNodes)).toBe(false)
+  const { nodes } = examplesCodeBaseResources(2).withoutCycle
+  expect(detectCycle(nodes)).toBe(false)
 })
